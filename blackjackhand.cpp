f51b6fd9bc7ecf81e4575accf3874e47d3c87633
Guney Kayim <guneykayim@gmail.com>
2019-02-22 00:08:18 +0000

    implemented score calculation to BlackjackHand class. Need to write unit tests!!

--- blackjackhand.cpp
+++ blackjackhand.cpp
@@ -2,9 +2,30 @@
 
 namespace CardGame {
     int BlackjackHand::getScore() const {
-      int v { 0 };
-      //TODO implement me
-      return v;
+      int score { 0 };
+      int numAces { 0 };
+
+      for(Card* c : this->cards) {
+        auto v = std::min<uint8_t>(10, c->getValue());
+        if(v == 1) { // ace
+          ++numAces;
+        } else {
+          score += v;
+        }
+      }
+
+      if (score > 10) {
+        score += numAces;
+      } else {
+        if(numAces > 0) {
+          score += 11 + numAces - 1;
+          if(score > this->blackjack) {
+            score -= 10;
+          }
+        }
+      }
+
+      return score;
     }
 
     bool BlackjackHand::isBlackJack() const {
